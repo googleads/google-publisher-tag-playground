ae9319fa0f86805d77e81a297c59a21eb098ce37
Jon Imperiosi <jimper@google.com>
2024-12-04 07:32:48 -0800

    Project import generated by Copybara
    PiperOrigin-RevId: 702719216

---
+++ src/generated/locales/zh-CN.ts
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Do not modify this file by hand!
+// Re-generate this file by running lit-localize
+
+import {str} from '@lit/localize';
+
+/* eslint-disable no-irregular-whitespace */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export const templates = {
+  s02c64e6a6306c0b0: '添加广告位',
+  s0536dee760be4bf2: '// 配置隐私设置。',
+  s08995feb9716ee48: '页外广告格式',
+  s09c694b6602d365d: '侧边栏广告（左侧）',
+  s0d576da69783c058: str`如果网页或设备不支持${0}广告位，这些广告位会返回 null。`,
+  s0eb106f4b9813ad3: '非个性化广告',
+  s1163c710c6c9bccf: '游戏插页式广告',
+  s11a3c998dc1af0b1: '广告示例',
+  s1c59de6a71660855: '添加尺寸',
+  s267a52c34c200ef0: '单一请求架构 (SRA)',
+  s292d08a9cea72454: '自适应广告',
+  s2d647200b120ea41: '广告单元路径',
+  s3132d1a0580760bc: '面向儿童的内容',
+  s3400fb34f1e245a9: '多尺寸广告',
+  s34774a88f199f364: '// 配置网页级定位。',
+  s352f8b213acc822a: str`${0}已加载完毕。`,
+  s3be169991df10a5d: '打开预览页面',
+  s3e0b42b56e05829d: '尺寸',
+  s4121536e35beb2ed: '固定尺寸广告 (728x90)',
+  s43e9aef7aa01fca9: '不可用',
+  s4535f519744108bf: '输出格式',
+  s4661d2a8c1259e58: '广告示例（页外）',
+  s48390e2c954ba495: '固定尺寸广告 (100x100)',
+  s506b6a19d12f414c: '键',
+  s57472465a23b41db: '值',
+  s5bf3f3d517569ebd: '锚定广告（底部）',
+  s5e13187e6961d8b1: '// 定义页外广告位。',
+  s5ec0955e7a33abf4: '// 启用单一请求模式。',
+  s5fc316a7f287b7be: '此示例无法在 iframe 中预览',
+  s669b18c6d2d9c95b: '无',
+  s703415876ae4626f: '固定尺寸广告 (750x200)',
+  s707a334b3b2a7140: str`${0}。点击下方的按钮即可在新窗口中预览。`,
+  s710f900ef624e363: '锚定广告（顶部）',
+  s716e903e2945c8cb: '移除定位值',
+  s71e012bd812c44a2: '// 定义静态广告位。',
+  s758d8c59379ea482: '网页级定位',
+  s77b0113eae08368b: '示例模板',
+  s7b5fa742d13b462e: '广告位设置',
+  s7d28e0b73aced2d9: '示例配置',
+  s8233e942045a2b50: str`此页面不支持${0}。`,
+  s8a2c4a3aa651b7fb: '移除广告位',
+  s90465cbd227840a3: str`${0}滚动页面即可激活。`,
+  s9691600b4b891cf3: '自适应',
+  s9d21580cc7643953: str`${0}正在加载…`,
+  sb457d189f407a5a6: '限制数据处理',
+  sb55d0a3abebf97fe: '移除尺寸',
+  sbba26a884a26c0c7: '固定尺寸广告 (300x250)',
+  sc5c8140d066b7231: '// 启用 GPT 服务。',
+  sc907ffeeafb021d1: '输出设置',
+  sccb7f7246c10ecb6: '侧边栏广告（右侧）',
+  sd3926a53f7b06107: '// 请求并呈现所有之前定义的广告位。',
+  sd5ac53cfd233dbcb: '受限广告',
+  se6dd79643508bb80: '定位',
+  se78c02e9a98a0f47: '隐私权',
+  secff207571282402: '网站插页式广告',
+  sed09c5dfcd2f8bc8: '添加定位键',
+  sefe480470a2c783c: '激励广告',
+  sf0cab2483f66aa2e: '自定义',
+  sf2057da4d0abd00e: '添加定位值',
+  sf21fb4f4b9e8911f: '宽度',
+  sf915707accde585c: '未达到同意年龄',
+  sf93d227444bc5687: '网页设置',
+  sffdadf0a576fd802: '高度',
+};
---
+++ src/generated/locales/zh-TW.ts
@@ -0,0 +1,89 @@
+/**
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Do not modify this file by hand!
+// Re-generate this file by running lit-localize
+
+import {str} from '@lit/localize';
+
+/* eslint-disable no-irregular-whitespace */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export const templates = {
+  s02c64e6a6306c0b0: '新增版位',
+  s0536dee760be4bf2: '// 調整隱私權設定。',
+  s08995feb9716ee48: '非頁內廣告格式',
+  s09c694b6602d365d: '側邊欄廣告 (左)',
+  s0d576da69783c058: str`如果網頁或裝置不支援，「${0}」廣告版位會傳回空值。`,
+  s0eb106f4b9813ad3: '非個人化廣告',
+  s1163c710c6c9bccf: '遊戲插頁式廣告',
+  s11a3c998dc1af0b1: '廣告範例',
+  s1c59de6a71660855: '新增大小',
+  s267a52c34c200ef0: '單一請求架構 (SRA)',
+  s292d08a9cea72454: '自動調整廣告',
+  s2d647200b120ea41: '廣告單元路徑',
+  s3132d1a0580760bc: '兒童導向內容',
+  s3400fb34f1e245a9: '多重大小廣告',
+  s34774a88f199f364: '// 設定網頁層級指定目標。',
+  s352f8b213acc822a: str`已載入${0}。`,
+  s3be169991df10a5d: '開啟預覽',
+  s3e0b42b56e05829d: '大小',
+  s4121536e35beb2ed: '固定大小廣告 (728x90)',
+  s43e9aef7aa01fca9: '無法使用',
+  s4535f519744108bf: '輸出格式',
+  s4661d2a8c1259e58: '廣告範例 (非頁內廣告)',
+  s48390e2c954ba495: '固定大小廣告 (100x100)',
+  s506b6a19d12f414c: '鍵',
+  s57472465a23b41db: '值',
+  s5bf3f3d517569ebd: '錨定廣告 (底部)',
+  s5e13187e6961d8b1: '// 定義非頁內廣告版位。',
+  s5ec0955e7a33abf4: '// 啟用單一請求模式。',
+  s5fc316a7f287b7be: '無法在 iframe 中預覽這個範例',
+  s669b18c6d2d9c95b: '無',
+  s703415876ae4626f: '固定大小廣告 (750x200)',
+  s707a334b3b2a7140: str`${0}。點選下方按鈕即可在新視窗中預覽。`,
+  s710f900ef624e363: '錨定廣告 (頂端)',
+  s716e903e2945c8cb: '移除指定值',
+  s71e012bd812c44a2: '// 定義靜態廣告版位。',
+  s758d8c59379ea482: '網頁層級指定目標',
+  s77b0113eae08368b: '範本範例',
+  s7b5fa742d13b462e: '版位設定',
+  s7d28e0b73aced2d9: '設定範例',
+  s8233e942045a2b50: str`這個頁面不支援${0}。`,
+  s8a2c4a3aa651b7fb: '移除版位',
+  s90465cbd227840a3: str`${0}捲動頁面即可啟用。`,
+  s9691600b4b891cf3: '自動調整',
+  s9d21580cc7643953: str`正在載入${0}...`,
+  sb457d189f407a5a6: '受限的資料處理',
+  sb55d0a3abebf97fe: '移除大小',
+  sbba26a884a26c0c7: '固定大小廣告 (300x250)',
+  sc5c8140d066b7231: '// 啟用 GPT 服務。',
+  sc907ffeeafb021d1: '輸出內容設定',
+  sccb7f7246c10ecb6: '側邊欄廣告 (右)',
+  sd3926a53f7b06107: '// 請求及顯示先前定義的所有廣告版位。',
+  sd5ac53cfd233dbcb: '受限制的廣告',
+  se6dd79643508bb80: '指定目標',
+  se78c02e9a98a0f47: '隱私權',
+  secff207571282402: '網站插頁式廣告',
+  sed09c5dfcd2f8bc8: '新增指定鍵',
+  sefe480470a2c783c: '獎勵廣告',
+  sf0cab2483f66aa2e: '自訂',
+  sf2057da4d0abd00e: '新增指定值',
+  sf21fb4f4b9e8911f: '寬度',
+  sf915707accde585c: '未滿規定年齡',
+  sf93d227444bc5687: '網頁設定',
+  sffdadf0a576fd802: '高度',
+};
